1280696b8e57712b89fc4010837c0e9ca3657875
sean-waev <98407121+sean-waev@users.noreply.github.com>
2025-03-14 10:39:25 -0400

    added single list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { ProductCardComponentComponent } from './product-card-component/product-card-component.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ProductCardComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/product-card-component/product-card-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card-component',
+  imports: [],
+  templateUrl: './product-card-component.component.html',
+  styleUrl: './product-card-component.component.css'
+})
+export class ProductCardComponentComponent {
+product = "Product 2"
+price = "4.99"
+}
